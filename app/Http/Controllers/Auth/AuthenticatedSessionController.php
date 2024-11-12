6fbda2c4a62a31cd12ecdf3c3ce2f72ae1960079
AlejandroMBP <marcosberriosal@gmail.com>
2024-11-12 06:11:40 -0400

    asignacion de inicion de sesion

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,9 +27,25 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+
+        // dd($request->all());
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        $userId = auth()->user()->id;
+
+        $roleId = DB::table('model_has_roles')
+            ->where('model_id', $userId)
+            ->where('model_type', 'App\Models\User')
+            ->value('role_id');
+
+        // dd($roleId);
+        $roleName = Role::find($roleId)->name;
+        if ($roleName == 'user') {
+            return redirect(RouteServiceProvider::ALERNATIVE);
+        }
+        // dd($roleName);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
